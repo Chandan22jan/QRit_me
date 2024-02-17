c495c5b7b1cb860047fce33f721e87c2fe44fc35
Chandan22jan <ckg.gkp786@gmail.com>
2024-02-17 18:27:23 +0530

    auth login css

--- src/Components/auth/screens/Auth.jsx
+++ src/Components/auth/screens/Auth.jsx
@@ -1,25 +1,67 @@
-import React from 'react'
-import { useState } from 'react';
+// import React from 'react'
+// import { useState } from 'react';
+// import AuthForm from '../widgets/AuthForm';
+// import '../screens/auth.css'
+
+// function Auth() {
+
+//   const [nameShouldBeShown, setNameShouldBeShown] = useState(false);
+//   const [buttonName, setButtonName] = useState('Signin');
+
+//   const [auth, setauth] = useState('signin');
+//   const handleChange = e => {
+//     const target = e.target;
+//     if (target.checked) {
+//       setauth(target.value);
+//       // console.log(auth);
+//       if (target.value === 'signup') {
+//         setNameShouldBeShown(true);
+//         setButtonName('Signup');
+//       }
+//       else {
+//         setNameShouldBeShown(false);
+//         setButtonName('Signin');
+//       }
+//     }
+//   };
+
+//   return (
+//     <div className='container'>
+//       <div className='Wrapper'>
+//         <h1>Welcome to Qrit</h1>
+//         <div className='Center'>
+//             <label><input type="radio" name="auth" value="signup" checked={auth === 'signup'} onChange={handleChange} /> Sign up</label>
+//             <br></br>
+//             <label><input type="radio" name="auth" value="signin" checked={auth === 'signin'} onChange={handleChange} /> Login</label>
+//             <AuthForm nameShouldBeShown={nameShouldBeShown} buttonName={buttonName}></AuthForm>
+//             <br></br>
+//         </div>
+//       </div> </div>
+
+//   )
+// }
+
+// export default Auth
+
+
+
+import React, { useState } from 'react';
 import AuthForm from '../widgets/AuthForm';
-import '../screens/auth.css'
+import '../screens/auth.css';
 
 function Auth() {
+  const [nameShouldBeShown, setNameShouldBeShown] = useState(false);
+  const [buttonName, setButtonName] = useState('Signin');
+  const [auth, setAuth] = useState('signin');
 
-    const [nameShouldBeShown,setNameShouldBeShown] = useState(false);
-    const [buttonName,setButtonName] = useState('Signin');
-
-const [auth, setauth] = useState('signin');
-  const handleChange = e => {
+  const handleChange = (e) => {
     const target = e.target;
     if (target.checked) {
-      setauth(target.value);
-      // console.log(auth);
-      if(target.value==='signup')
-      {
+      setAuth(target.value);
+      if (target.value === 'signup') {
         setNameShouldBeShown(true);
         setButtonName('Signup');
-      }
-      else{
+      } else {
         setNameShouldBeShown(false);
         setButtonName('Signin');
       }
@@ -27,19 +69,26 @@ const [auth, setauth] = useState('signin');
   };
 
   return (
-    <div className='Wrapper'>
-      <div className='Center'>
+    <div className='container'>
+      <div className='Wrapper'>
         <h1>Welcome to Qrit</h1>
-            <div className='Selec'>
-              <label className='Rbtn'><input type="radio" name="auth" value="signup" checked={auth === 'signup'} onChange={handleChange} /> Sign up</label>
-              <br></br>
-              <label className='Rbtn'><input type="radio" name="auth" value="signin" checked={auth === 'signin'} onChange={handleChange}/> Login</label>
-              <AuthForm nameShouldBeShown={nameShouldBeShown} buttonName={buttonName}></AuthForm>
-              <br></br>
-            </div>
+        <div className='Center'>
+          <label className="radio-label">
+            <input type="radio" name="auth" value="signup" checked={auth === 'signup'} onChange={handleChange} />
+            <span>Sign up</span>
+          </label>
+          <br />
+          <label className="radio-label">
+            <input type="radio" name="auth" value="signin" checked={auth === 'signin'} onChange={handleChange} />
+            <span>Login</span>
+          </label>
+          <AuthForm nameShouldBeShown={nameShouldBeShown} buttonName={buttonName}></AuthForm>
+          <br />
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Auth
+export default Auth;
+
